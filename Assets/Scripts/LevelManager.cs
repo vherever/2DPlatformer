a60094085709c02b4253a5ac9e2acfc9139c1a5d
Vladimir <vromanyuk@it7.com.ua>
2016-01-02 00:26:07 +0200

    particles

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,11 @@ public class LevelManager : MonoBehaviour {
 
 	private PlayerController player;
 
+	public GameObject deathParticle;
+	public GameObject respawnParticle;
+
+	public float respawnDelay;
+
 	// Use this for initialization
 	void Start () {
 		player = FindObjectOfType<PlayerController> ();
@@ -18,7 +23,20 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public void RespawnPlayer() {
+		StartCoroutine ("RespawnPlayerGo");
+	}
+
+	public IEnumerator RespawnPlayerGo() {
+		Instantiate (deathParticle, player.transform.position, player.transform.rotation);
+		player.enabled = false;
+		player.GetComponent<Renderer>().enabled = false;
+		//gameObject.active = false;
 		Debug.Log ("Player Respawn");
+		yield return new WaitForSeconds (respawnDelay);
 		player.transform.position = currentCheckpoint.transform.position;
+		player.enabled = true;
+		player.GetComponent<Renderer>().enabled = true;
+		Instantiate (respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+
 	}
 }
